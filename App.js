6300f8b6e8cfee20ba656767b20a8ee87234f30d
alberthuynh91 <alberthuynh91@gmail.com>
2018-03-28 22:59:27 -0700

    Got image and some text

--- App.js
+++ App.js
@@ -1,13 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView } from 'react-native';
+
+let pic = {
+  uri: 'https://res.cloudinary.com/wheelchimp/image/upload/v1518503938/nz7vzot8xzmxgajn0pur.jpg'
+};
+
+let data = {
+  title: 'Work Equip 18x9.5 +20 18x10.5 +20',
+  description: 'Brand/Model: Work Equip -Bolt Pattern: 5x114.3 -Front: 18x9.5+20 -Rear: 18x10.5+20 -Face: Chrome (original from work) -Price: $2600 NO TRADE NO TRADE -Contact: 626 551 132 ZERO **Gold hardware available for additional cost** **Want a different lug pattern? can also redrill these** See original listing here: http://zilvia.net/f/showthread.php?t=647958'
+}
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+      <ScrollView contentContainerStyle={styles.contentContainer}>
+        <View style={{
+          justifyContent: 'center',
+          alignItems: 'center',
+          marginTop: 20
+        }}>
+          <Image source={pic} style={{width: 193, height: 110}}/>
+        </View>
+
+        <Text style={{fontWeight: 'bold'}}>
+          {data.title}
+        </Text>
+
+        <Text style={{marginTop: 20}}>
+          {data.description}
+        </Text>
+
+
+        
+      </ScrollView>
+        
       </View>
     );
   }
@@ -20,4 +47,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  contentContainer: {
+    paddingVertical: 20
+  }
 });
