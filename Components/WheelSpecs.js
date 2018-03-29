e5e117c8dc757ad4b96127a2a8b7d20086dc3bb4
alberthuynh91 <alberthuynh91@gmail.com>
2018-03-29 00:47:10 -0700

    Add wheelspecs component

---
+++ Components/WheelSpecs.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { View, ScrollView, Image, StyleSheet, Dimensions } from 'react-native';
+
+const { width } = Dimensions.get('window');
+const height = width * 0.8;
+
+export default class WheelSpecs extends Component {
+  render() {
+    const { images } = this.props;
+    if (images && images.length) {
+    return (
+        <View
+        style={styles.scrollContainer}
+        >
+        </View>
+   
+    );
+    }
+  }
+}
+
+const styles = StyleSheet.create({
+  scrollContainer: {
+    height,
+  },
+  image: {
+    width,
+    height,
+  },
+});
