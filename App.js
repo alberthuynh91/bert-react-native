86c85a7cc3cb8389865b6f544da51353c3d6b722
alberthuynh91 <alberthuynh91@gmail.com>
2018-03-29 00:30:44 -0700

    Added a working carousel

--- App.js
+++ App.js
@@ -2,16 +2,51 @@ import React from 'react';
 import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity, Dimensions } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
-let pic = {
-  uri: 'https://res.cloudinary.com/wheelchimp/image/upload/v1518503938/nz7vzot8xzmxgajn0pur.jpg'
-};
+const images = [
+  {
+    uri: 'https://res.cloudinary.com/wheelchimp/image/upload/v1518503938/nz7vzot8xzmxgajn0pur.jpg'
+  },
+  {
+    uri: 'https://res.cloudinary.com/wheelchimp/image/upload/v1518503938/nz7vzot8xzmxgajn0pur.jpg'
+  },
+  {
+    uri: 'https://res.cloudinary.com/wheelchimp/image/upload/v1518503948/edz5c3wrylkn6ugzt5sz.jpg'
+  }
+]
 
 let data = {
   title: 'Work Equip 18x9.5 +20 18x10.5 +20',
   description: 'Brand/Model: Work Equip -Bolt Pattern: 5x114.3 -Front: 18x9.5+20 -Rear: 18x10.5+20 -Face: Chrome (original from work) -Price: $2600 NO TRADE NO TRADE -Contact: 626 551 132 ZERO **Gold hardware available for additional cost** **Want a different lug pattern? can also redrill these** See original listing here: http://zilvia.net/f/showthread.php?t=647958'
 }
 
+const dimensions = Dimensions.get('window');
+const { width } = Dimensions.get('window');
+const height = dimensions.width * 0.8;
+const imageHeight = Math.round(dimensions.width * 9 / 16);
+const imageWidth = dimensions.width;
+
 export default class App extends React.Component {
+
+  getCarousel = () => {
+    if (images && images.length) {
+      return (
+        <View
+          style={styles.scrollContainer}
+        >
+          <ScrollView
+            horizontal
+            pagingEnabled
+            showsHorizontalScrollIndicator={false}
+          >
+            {images.map(image => (
+              <Image style={styles.image} source={image} />
+            ))}
+          </ScrollView>
+        </View>
+      );
+    }
+  }
+
   onPress = () => {
     this.setState({
       count: this.state.count+1
@@ -19,9 +54,9 @@ export default class App extends React.Component {
   }
 
   render() {
-    const dimensions = Dimensions.get('window');
-    const imageHeight = Math.round(dimensions.width * 9 / 16);
-    const imageWidth = dimensions.width;
+    
+
+    
 
     return (
       <View style={styles.container}>
@@ -32,7 +67,7 @@ export default class App extends React.Component {
           alignItems: 'center',
           marginTop: 20
         }}>
-          <Image source={pic} style={{width: imageWidth, height: imageHeight}}/>
+          {this.getCarousel()}
         </View>
 
         <View style={styles.touchContainer}>
@@ -86,5 +121,12 @@ const styles = StyleSheet.create({
     paddingTop: 15,
     paddingBottom: 15,
     paddingLeft: 23,
+  },
+  scrollContainer: {
+    height,
+  },
+  image: {
+    width,
+    height,
   }
 });
